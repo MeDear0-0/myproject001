3cbd61323257599a88b043e23cb029c7e1da3eb2
MeDear0-0 <pimpajee.seth@mail.kmutt.ac.th>
2023-03-27 17:35:08 +0700

    filenew

--- MyProject2.java
+++ MyProject2.java
@@ -4,7 +4,7 @@ public class MyProject2 {
   public static void main(String[] arg){
       System.out.println("HELLO Pimpajee");
       System.out.println("HELLO Java1!");
-      System.out.println("HELLO Java1!");
+      System.out.println("HELLO Java!");
   }
 }
 
