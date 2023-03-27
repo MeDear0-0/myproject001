debf4af1a99e5194766a1f6197d13582d2c0d8cc
MeDear0-0 <pimpajee.seth@mail.kmutt.ac.th>
2023-03-27 15:18:51 +0700

    newbb

--- MyProject2.java
+++ MyProject2.java
@@ -3,7 +3,8 @@ package myproject2;
 public class MyProject2 {
   public static void main(String[] arg){
       System.out.println("HELLO Pimpajee");
-      System.out.println("HELLO Java");
+      System.out.println("HELLO Java1!");
+      System.out.println("HELLO Java1!");
   }
 }
 
